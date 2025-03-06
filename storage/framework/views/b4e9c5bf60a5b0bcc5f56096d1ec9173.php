2fb4bda03f38ba1cd1d756ef330564e9d5d1fbb8
HOFII / TUNA <145539219+HOFii@users.noreply.github.com>
2025-03-06 11:16:44 +0700

    Update Storages

---
+++ storage/framework/views/b4e9c5bf60a5b0bcc5f56096d1ec9173.php
@@ -0,0 +1,6 @@
+<footer class="c-footer">
+    <div>Triangle Pos © <?php echo e(date('Y')); ?> || Modify By <strong><a target="_blank" href="https://github.com/HOFii">HOFii</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
+<?php /**PATH C:\Users\HOFi Desuka\triangle-pos\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
