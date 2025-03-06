c04260839146837e983243bc15ac4142366ef8f5
HOFII / TUNA <145539219+HOFii@users.noreply.github.com>
2025-03-06 11:15:30 +0700

    Update Resources

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Triangle Pos © {{ date('Y') }} || Modify By <strong><a target="_blank" href="https://github.com/HOFii">HOFii</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
